a187c02549d785686fb5a5c01f8dcf3ad74564d7
jinyeow <jin-yeow@outlook.com>
2016-10-31 15:43:00 +1100

    started on a url building utility to scrape the chapters

---
+++ lib/novel_reader/utilities/url_builder.ex
@@ -0,0 +1,8 @@
+defmodule NovelReader.Utilities.URLBuilder do
+
+  def build_url(novel_name, chapter, site \\ "") do
+    # from novel_name find corresponding site from db/ecto/ets/file
+    # url = site + name + chapter
+    # return url
+  end
+end
---
+++ test/novel_reader/utilities/url_builder_test.exs
@@ -0,0 +1,8 @@
+defmodule NovelReader.Utilities.URLBuilderTest do
+  use ExUnit.Case, async: true
+
+  # TODO
+  test "it builds a chapter's url from the novel name and chapter number" do
+    assert false
+  end
+end
