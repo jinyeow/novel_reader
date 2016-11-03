6ebd69529d2fb2826156081eb5a05c6e9439ab4f
jinyeow <jin-yeow@outlook.com>
2016-11-03 18:16:57 +1100

    added supervisor to novel_reader.ex

--- lib/novel_reader.ex
+++ lib/novel_reader.ex
@@ -1,2 +1,15 @@
 defmodule NovelReader do
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      # supervisor(Task.Supervisor, [[name: NovelReader.TaskSupervisor]]),
+      # worker(NovelReader.FeedServer, [])
+    ]
+
+    opts = [strategy: :one_for_one, name: NovelReader.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
 end
