2a85981555060c4881d083b6991ddac231b06bef
jinyeow <jin-yeow@outlook.com>
2016-11-07 21:40:13 +1100

    added novel_updates_test.exs test file to add tests for NovelUpdates API etc.

---
+++ test/novel_reader/novel_updates_test.exs
@@ -0,0 +1,6 @@
+defmodule NovelReader.NovelUpdatesTest do
+  use ExUnit.Case, async: true
+
+  # TODO add tests for the GenServer.handle_[call|cast] functions.
+  # This should work fine as the "API" just passes thru to these functions.
+end
