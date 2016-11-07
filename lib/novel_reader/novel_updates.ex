a8c03e0554b9d3a17be3deb52928a90d3c32c7db
jinyeow <jin-yeow@outlook.com>
2016-11-07 15:03:10 +1100

    added search/filter and change feed url functions to NovelReader.NovelUpdates module

--- lib/novel_reader/novel_updates.ex
+++ lib/novel_reader/novel_updates.ex
@@ -13,19 +13,48 @@ defmodule NovelReader.NovelUpdates do
   # Callbacks
 
   def start_link(feed_url \\ @feed) do
-    GenServer.start_link(__MODULE__, {feed_url, []}, name: @name)
+    case GenServer.start_link(__MODULE__, {feed_url, []}, name: @name) do
+      {:ok, pid} ->
+        Logger.info "Started #{__MODULE__}"
+        {:ok, pid}
+      {:error, {:already_started, pid}} ->
+        Logger.info "#{__MODULE__} already started"
+        {:ok, pid}
+    end
   end
 
   def get_updates do
     GenServer.call(@name, :get_updates)
   end
 
+  def filter_updates(title) do
+    GenServer.call(@name, {:filter_updates, title})
+  end
+
+  def change_feed(feed) do
+    GenServer.call(@name {:change_feed, feed})
+    get_updates
+  end
+
   # Server
 
-  def handle_call(:get_updates, _from, {feed_url, _chapter_updates}) do
+  def handle_call(:get_updates, _from, {feed_url, _chapters}) do
     updates = feed_url
     |> Scrape.feed
     {:reply, updates, {feed_url, updates}}
+  end
+
+  def handle_call({:filter_updates, title}, _from, {_feed_url, chapters} = state) do
+    {:ok, pattern} = Regex.compile(title, "i")
+    filtered_chapters = chapters
+                        |> Enum.filter(fn chapter ->
+                          Regex.match?(pattern, chapter[:title])
+                        end)
+    {:reply, filtered_chapters, state}
+  end
+
+  def handle_call({:change_feed, feed}, _from, {_feed_url, chapters}) do
+    {:noreply, {feed, chapters}}
   end
 end
 
