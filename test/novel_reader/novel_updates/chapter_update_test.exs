51a4592d1b09df8e351ab605ed1dd45e8f65f523
jinyeow <jin-yeow@outlook.com>
2016-11-08 13:58:50 +1100

    added test file for ChapterUpdate

---
+++ test/novel_reader/novel_updates/chapter_update_test.exs
@@ -0,0 +1,9 @@
+defmodule NovelReader.NovelUpdates.ChapterUpdateTest do
+  use ExUnit.Case, async: true
+  doctest NovelReader.NovelUpdates.ChapterUpdate
+
+  # TODO add tests
+  test "1 == 2" do
+    assert 1 == 2, "Add tests for ChapterUpdate"
+  end
+end
