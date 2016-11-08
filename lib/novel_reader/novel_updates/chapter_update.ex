07e1966be5b6884bead9aac4a5ec3d39532cbf16
jinyeow <jin-yeow@outlook.com>
2016-11-08 17:39:05 +1100

    skeleton for Retriever module used to pull chapter text - each 'pulling' method will differ depending on the respective translators site. use different 'formats'

--- lib/novel_reader/novel_updates/chapter_update.ex
+++ lib/novel_reader/novel_updates/chapter_update.ex
@@ -2,4 +2,5 @@ defmodule NovelReader.NovelUpdates.ChapterUpdate do
   defstruct [:chapter_url, :pubdate, :series_url, :tags, :title, :translator]
 
   # TODO add functions to get/set struct fields
+  # TODO add specs/types for each field
 end
---
+++ lib/novel_reader/retriever.ex
@@ -0,0 +1,13 @@
+defmodule NovelReader.Retriever do
+  @moduledoc """
+  This handles getting actual chapter content/text from the respective
+  translation websites (e.g. WuxiaWorld, XianXiaWorld, Gravity Tales).
+
+  It should store the chapter in [memory|text|ets] ?
+
+               |--TaskSupervisor := async tasks ?
+               |--NovelUpdates := communicate with NU and get chapter updates
+  NovelReader--|--GUI := display information using Electron
+               |--Retriever := pull chapter text
+  """
+end
