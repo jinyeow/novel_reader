329f404b82ccc612c2951c7a485a35a9a54239c9
jinyeow <jin-yeow@outlook.com>
2016-11-12 14:49:35 +1100

    added ExVCR to novel_updates_test.exs and added exvcr as a dependency in the mix.exs file

--- mix.exs
+++ mix.exs
@@ -11,25 +11,16 @@ defmodule NovelReader.Mixfile do
   end
 
   # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
+
   def application do
     [applications: [:logger, :scrape, :floki, :parallel, :timex],
      mod: {NovelReader, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:scrape, "~> 1.2"}
+      {:scrape, "~> 1.2"},
+      {:exvcr, "~> 0.8", only: :test}
     ]
   end
 
--- test/novel_reader/novel_updates_test.exs
+++ test/novel_reader/novel_updates_test.exs
@@ -1,15 +1,22 @@
 defmodule NovelReader.NovelUpdatesTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case, async: false
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
 
-  # TODO add tests for the GenServer.handle_[call|cast] functions.
-  # This should work fine as the "API" just passes thru to these functions.
-  # Use a Mocking library to test other functionality?
+  setup_all do
+    HTTPoison.start
+  end
 
   test "it is initialized with the default feed url" do
     assert NovelReader.NovelUpdates.feed == "http://www.novelupdates.com/rss.php?uid=12590&unq=571077742187a&type=read"
   end
 
-  test "it is initialized with an empty list of updates" do
-    assert NovelReader.NovelUpdates.updates == []
+  test "it is initialized with a non-empty list of updates" do
+    use_cassette "novel_updates/updates#1" do
+      refute NovelReader.NovelUpdates.updates == []
+    end
   end
+
+  # TODO add more tests for NovelReader.NovelUpdates
+  # TODO check that ExVCR is being used correctly
+
 end
