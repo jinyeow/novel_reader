411869ba37bc5abbd57adeb27a3bdae0642605d1
jinyeow <jin-yeow@outlook.com>
2016-11-13 17:21:25 +1100

    added a test for ChapterUpdate

--- test/novel_reader/novel_updates/chapter_update_test.exs
+++ test/novel_reader/novel_updates/chapter_update_test.exs
@@ -2,8 +2,28 @@ defmodule NovelReader.NovelUpdates.ChapterUpdateTest do
   use ExUnit.Case, async: true
   doctest NovelReader.NovelUpdates.ChapterUpdate
 
-  # TODO add tests
-  test "1 == 2" do
-    assert 1 == 2, "Add tests for ChapterUpdate"
+  alias NovelReader.NovelUpdates.ChapterUpdate
+
+  # TODO add more tests
+
+  test "is able to update ChapterUpdate attributes" do
+    updated_chapter = chapter
+    |> NovelReader.NovelUpdates.ChapterUpdate.update(
+      :tags,
+      chapter[:tags] ++ ["immortal"]
+    )
+
+    assert updated_chapter[:tags] == ["immortal"]
+  end
+
+  defp chapter do
+    %ChapterUpdate{
+      chapter_url: "http://www.novelupdates.com/extnu/340123/",
+      chapters: [163],
+      part: nil,
+      series_url: "http://www.noveludpates.com/series/spirit-realm/",
+      tags: [],
+      title: "Spirit Realm", translator: "Alyschu", volume: nil
+    }
   end
 end
