b1dd7784023d662f4c960dfc2cfa2226b1510b54
jinyeow <jin-yeow@outlook.com>
2016-12-12 18:15:31 +1100

    implemented all the get_{title,novel,chapter,next,prev} functions for the gravity tales retriever

--- lib/novel_reader/retriever/gravity_tales.ex
+++ lib/novel_reader/retriever/gravity_tales.ex
@@ -54,9 +54,12 @@ defmodule NovelReader.Retriever.GravityTales do
       body
       |> Floki.find(".entry-content a")
       |> Enum.filter(fn elem ->
-        elem
-        |> Floki.attribute("href")
-        |> hd =~ ~r/-chapter-[0-9]+/
+        with [href] <- elem
+                      |> Floki.attribute("href") do
+          href =~ ~r/-chapter-[0-9]+/
+        else
+          [] -> false
+        end
       end)
       |> hd
 
@@ -69,8 +72,12 @@ defmodule NovelReader.Retriever.GravityTales do
   def parse_chapter_page(page) do
     %HTTPoison.Response{body: body} = page
     %Chapter{
-      content: get_content(body)
-      # TODO get_{next,prev,novel,title,chapter}
+      chapter: get_chapter(body),
+      content: get_content(body),
+      next:    get_next(body),
+      novel:   get_novel(body),
+      prev:    get_prev(body),
+      title:   get_title(body)
     }
   end
 
@@ -81,5 +88,54 @@ defmodule NovelReader.Retriever.GravityTales do
       |> hd
 
     Floki.DeepText.get(content, "\n")
+  end
+
+  def get_novel(body) do
+    body
+    |> Floki.find("h3")
+    |> hd
+    |> Floki.text
+  end
+
+  def get_title(body) do
+    body
+    |> Floki.find("div.innerContent p")
+    |> hd
+    |> Floki.text
+    |> String.split(~r/[:\-] /, trim: true)
+    |> List.last
+  end
+
+  def get_chapter(body) do
+    "Chapter " <> num =
+      body
+      |> Floki.find("div.innerContent p")
+      |> hd
+      |> Floki.text
+      |> String.split(~r/[:\-] /, trim: true)
+      |> hd
+
+    String.trim(num)
+  end
+
+  def get_next(body) do
+    next =
+      body
+      |> Floki.find("div.chapter-navigation a")
+      |> List.last
+      |> Floki.attribute("href")
+      |> hd
+    @base_url <> next
+  end
+
+  def get_prev(body) do
+    prev =
+      body
+      |> Floki.find("div.chapter-navigation a")
+      |> hd
+      |> Floki.attribute("href")
+      |> hd
+    @base_url <> prev
+
   end
 end
