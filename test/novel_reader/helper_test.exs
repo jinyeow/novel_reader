d8d273340531e1b2833e241fea2447c68654b20e
jinyeow <jin-yeow@outlook.com>
2016-12-12 13:42:43 +1100

    added a test for NovelReader.Helper module

--- test/novel_reader/helper_test.exs
+++ test/novel_reader/helper_test.exs
@@ -1,3 +1,18 @@
 defmodule NovelReader.HelperTest do
   use ExUnit.Case, async: true
+
+  import NovelReader.Helper
+
+  test "valid_url?/1 returns true for a valid url" do
+    valid_urls = [
+      "http://gravitytales.com",
+      "http://www.wuxiaworld.com",
+      "https://www.google.com",
+      "http://www.xianxiaworld.net/The-Magus-Era/1000098.html"
+    ]
+
+    for url <- valid_urls do
+      assert valid_url?(url) == true
+    end
+  end
 end
